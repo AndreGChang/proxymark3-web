6b7f3a464fc2b9eef732a6fcfc6bdbedd5e9c5ae
AndreGChang <FireWitch2003@outlook.com>
2024-08-08 11:15:18 -0300

    estilização dos templates

--- app/routes.py
+++ app/routes.py
@@ -1,58 +1,3 @@
-# from flask import current_app as app, render_template, request, jsonify
-# import subprocess
-
-# # Array de pessoas
-# people = [
-#     {
-#         "uid": "08 11 33 3E",
-#         "name": "John Doe",
-#         "age": 30,
-#         "email": "johndoe@example.com"
-#     }
-# ]
-
-# @app.route('/')
-# def index():
-#     return render_template('index.html')
-
-# @app.route('/execute', methods=['POST'])
-# def execute():
-#     try:
-#         # Usar o caminho completo do script pm3
-#         result = subprocess.run(["/home/text/Documents/proxymark/proxmark3/pm3", "-c", "hf search"], capture_output=True, text=True)
-        
-#         # Verificar se a saída contém "UID" e retornar essa linha
-#         uid_line = ""
-#         for line in result.stdout.splitlines():
-#             if "UID" in line:
-#                 uid_line = line.strip()
-#                 break
-        
-#         print(f"UID line found: {uid_line}")  # Log para depuração
-
-#         # Extrair o UID da linha
-#         uid = uid_line.split(":")[1].strip() if uid_line else ""
-#         print(f"Extracted UID: {uid}")  # Log para depuração
-
-#         # Verificar se o UID corresponde a uma pessoa
-#         person = next((p for p in people if p["uid"] == uid), None)
-        
-#         if person:
-#             return jsonify(person)
-#         else:
-#             return "UID not found", 404
-#     except Exception as e:
-#         print(f"Error: {str(e)}")  # Log de erro
-#         return str(e), 500
-
-# @app.route('/person/<uid>')
-# def person(uid):
-#     person = next((p for p in people if p["uid"] == uid), None)
-#     if person:
-#         return render_template('person.html', person=person)
-#     else:
-#         return "Person not found", 404
-
 from flask import current_app as app, render_template, request, jsonify
 import subprocess
 
@@ -62,13 +7,22 @@ people = [
         "uid": "08 11 33 3E",
         "name": "John Doe",
         "age": 30,
-        "email": "johndoe@example.com"
+        "email": "johndoe@example.com",
+        "photo_path": "/static/images/Minecraft-creeper.avif"
     },
     {
         "uid": "08 11 33 3A",
         "name": "Kevin Yun",
         "age": 39,
-        "email": "kevinyun@example.com"
+        "email": "kevinyun@example.com",
+        "photo_path": "/static/images/Minecraft-creeper.avif"
+    },
+    {
+        "uid": "49 36 8A 93",
+        "name": "Joao Pedro",
+        "age": 20,
+        "email": "joaopedro@example.com",
+        "photo_path": "/static/images/Minecraft-creeper.avif"
     }
 ]
 
@@ -100,6 +54,14 @@ def execute():
         formatted_uid = " ".join(uid_parts)
         print(f"Extracted and formatted UID: {formatted_uid}")  # Log para depuração
 
+        #Cria arquivo com o UID da pessoa do cartão
+        try:
+            with open("uid_person.txt", "a") as file:
+                file.write(f"{formatted_uid}\n")
+                print("UID added to uid_person.txt")
+        except Exception as file_error:
+            print(f"Error writing to file: {str(file_error)}")
+    
         # Verificar se o UID corresponde a uma pessoa
         person = next((p for p in people if p["uid"] == formatted_uid), None)
         print(person)
@@ -118,15 +80,24 @@ def execute():
 def execute_dump():
     try:
         # Usar o caminho completo do script pm3 para o segundo comando
-        result = subprocess.run(["/home/text/Documents/proxymark/proxmark3/pm3", "-c",
-                                "hf mf dump -k /home/text/Documents/development/proxymark/hf-mf-key.bin -f teste.bin"], capture_output=True, text=True)
+        result = subprocess.run(["/home/text/Documents/proxymark/proxmark3/pm3", "-c", "hf mf dump -k /home/text/Documents/development/proxymark/hf-mf-key.bin -f dump/teste.bin"], capture_output=True, text=True)
 
-        print(result.stdout)  # Log para depuração
+        print(result.stdout)  # Log para depuraçã
+        
+        # Defina o usuário que irá executar o comando
+        user = "text"
+
+        # Comando a ser executado
+        find_command = [
+            "sudo",
+            "-u", user,
+            "find", "/home/text/dump", "-name", "test.bin", "-type", "f", "-delete"
+        ]
         
         # Procurar e deletar o arquivo test.bin
-        find_command = "find /home -name test.bin -type f -delete"
-        find_result = subprocess.run(
-            find_command, shell=True, capture_output=True, text=True)
+        #find_command = "sudo -u findeuser find /home/text -name test.bin -type f -delete"
+        find_result = subprocess.run(find_command, capture_output=True, text=True)
+        
         if find_result.returncode == 0:
             print("Arquivo test.bin deletado com sucesso")
         else:
@@ -137,6 +108,31 @@ def execute_dump():
         print(f"Error: {str(e)}")  # Log de erro
         return str(e), 500
 
+    
+@app.route('/execute_clone', methods=['POST'])
+def execute_clone():
+    try:
+        # Ler o UID do arquivo uid_person.txt
+        with open("uid_person.txt", "r") as file:
+            last_uid = file.readlines()[-1].strip()
+        print(f"Last UID from file: {last_uid}")
+
+        # Comando para clonar o cartão
+        clone_command = [
+            "/home/text/Documents/proxymark/proxmark3/pm3",
+            "-c",
+            f"hf mf cload -f dump/test.bin -u {last_uid}"
+        ]
+
+        clone_result = subprocess.run(
+            clone_command, capture_output=True, text=True)
+        print(clone_result.stdout)  # Log para depuração
+
+        return clone_result.stdout if clone_result.stdout else "No output from clone command"
+    except Exception as e:
+        print(f"Error: {str(e)}")  # Log de erro
+        return str(e), 500
+    
 
 @app.route('/person')
 def person():
--- app/static/js/script.js
+++ app/static/js/script.js
@@ -13,9 +13,36 @@ async function handleFormSubmit(event, url) {
         document.getElementById('loading').style.display = 'none';
 
         if (response.ok) {
-            const result = await response.text();
-            console.log(result);
-            document.getElementById('output').innerText = result;
+            const data = await response.json();
+            console.log(data);
+
+            if (data.error) {
+                document.getElementById('output').innerText = data.error;
+            } else {
+                const outputDiv = document.getElementById('output');
+                outputDiv.innerHTML = `
+                    <div class="details-section">
+                        <img src="${data.photo_path}" alt="Foto" class="image-avatar">
+                        <div class="info">
+                            <p><strong>Nome:</strong> ${data.name}</p>
+                            <p><strong>Idade:</strong> ${data.age}</p>
+                            <p><strong>Email:</strong> ${data.email}</p>
+                            <p><strong>UID:</strong> ${data.uid}</p>
+                        </div>
+                    </div>
+                    <div class="json-section">
+                        <pre>
+{
+    "age": ${data.age},
+    "email": "${data.email}",
+    "name": "${data.name}",
+    "uid": "${data.uid}",
+    "photo_path": "${data.photo_path}"
+}
+                        </pre>
+                    </div>
+                `;
+            }
         } else {
             const result = await response.text();
             console.log(`Error response: ${result}`);
@@ -34,4 +61,55 @@ document.getElementById('commandForm1').addEventListener('submit', function (eve
 
 document.getElementById('commandForm2').addEventListener('submit', function (event) {
     handleFormSubmit(event, '/execute_dump');
+});
+
+document.getElementById('commandForm3').addEventListener('submit', function (event) {
+    handleFormSubmit(event, '/execute_clone');
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelectorAll('form').forEach(form => {
+        form.addEventListener('submit', function (event) {
+            event.preventDefault();
+            const formData = new FormData(this);
+            fetch(this.action, {
+                method: this.method,
+                body: formData
+            })
+                .then(response => response.json())
+                .then(data => {
+                    if (data.error) {
+                        document.querySelector('.output').innerHTML = `<p>${data.error}</p>`;
+                    } else {
+                        const outputDiv = document.querySelector('.output');
+                        outputDiv.innerHTML = `
+                        <div class="details-section">
+                            <img src="${data.photo_path}" alt="Foto" class="image-avatar">
+                            <div class="info">
+                                <p><strong>Nome:</strong> ${data.name}</p>
+                                <p><strong>Idade:</strong> ${data.age}</p>
+                                <p><strong>Email:</strong> ${data.email}</p>
+                                <p><strong>UID:</strong> ${data.uid}</p>
+                            </div>
+                        </div>
+                        <div class="json-section">
+                            <pre>
+{
+    "age": ${data.age},
+    "email": "${data.email}",
+    "name": "${data.name}",
+    "uid": "${data.uid}",
+    "photo_path": "${data.photo_path}"
+}
+                            </pre>
+                        </div>
+                    `;
+                    }
+                })
+                .catch(error => {
+                    document.querySelector('.output').innerHTML = `<p>Error: ${error.message}</p>`;
+                    console.error('Error:', error);
+                });
+        });
+    });
 });
